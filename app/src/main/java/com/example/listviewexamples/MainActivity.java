de69a6caef74ca2ab95ae1e605ea7904ce9bc9a8
Dynamo <yharendra7864@gmail.com>
2019-09-15 01:20:03 -0400

    15-09-2019 mainactivity

--- app/src/main/java/com/example/listviewexamples/MainActivity.java
+++ app/src/main/java/com/example/listviewexamples/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
     }
